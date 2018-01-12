2f5d2585b4c35f421f04298907e4e277de495de9
Your Name <you@example.com>
2018-01-11 18:48:34 -0800

    book_app table update

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
 'use strict';
-
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
-
 const cors =require('cors');
-
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-
-
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/postgres';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/books_app';
 const client = new pg.Client(DATABASE_URL);
 client.connect();
+app.use(cors());
+app.get(`/api/v1/books`,(req,res) =>{
+  client.query(`
+  SELECT book_id, author, title, image_url FROM books;
+`).then(result => res.send(result.rows))
+    .catch(err => console.error(err))
+})
 client.on('error', err => {
   console.error(err);
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('/public'));
-
-app.use(cors());
-
-// app.get('/index', (req, res) => res.send('hello world'))
-app.get('/index', (req, res) => res.sendFile('index.html', {root: '/MICHAEL-IXIUS/client/public'}))
-
-app.get('/api/vi/books', (req,res) => {
-    client.query(`SELECT book_id,title,author,image_url FROM book_app`)
-    .then(result => response.send(results.rows)).catch(console.error);
-    console.log('query here',response)
-});
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
